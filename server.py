2671cceea4de3fcab69693e990f177ed5878b320
maryam <m.shakerimaryam@gmail.com>
2023-04-13 16:25:31 +0200

    Delete Bootstrap

--- server.py
+++ server.py
@@ -45,8 +45,7 @@ db = SQLAlchemy()
 # ---------------------------------------- Flask Server Creation ----------------------------------------
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-Bootstrap(app)
+
 
 """ ---------------------------------------- SQLite database Creation ----------------------------------------
 ------------ For books"""
